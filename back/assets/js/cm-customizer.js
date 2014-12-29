97a6df6e819dc3ca232231c10843ca309fe39686
Rocco Aliberti <raliberti84@gmail.com>
2014-12-29 12:36:18 +0100

    Pre-release

---
+++ back/assets/js/cm-customizer.js
@@ -0,0 +1,107 @@
+/*! Childify Me plugin by Rocco Aliberti, GPL2+ licensed */
+/*
+ * TODO: 
+ * - better errors handling?
+**/
+jQuery(function ($) {
+    var $main_panel = $('#customize-info').parent(),
+        childify_template = _.template(
+            $('script#childify-tpl').html()
+        ),
+        action  = "action="+CMAdmin.Action,
+        nonce   = "CMnonce="+CMAdmin.CMnonce,
+        tparent  = "parent="+CMAdmin.Parent,
+        data    = action+"&"+nonce+"&"+tparent,
+        ajaxurl = CMAdmin.AjaxUrl;
+    
+    $main_panel.append( childify_template() );
+    
+    $('#cm-add-new').click(function(){
+        $('#cm-form-container').slideToggle("fast");
+    });
+    
+    $('#cm-cancel').click(function(){
+        $('#cm-cname').removeAttr('readonly');
+        $('#cm-create').removeAttr('disabled');
+        $('#cm-cname').val("");
+        detach_ftp_form();
+    });
+    
+    $('#cm-create').click(function(){
+        if ( ! validate() )
+            return;
+        $(this).attr('disabled', 'disabled');
+        $('#cm-cname').attr('readonly', 'readonly');
+        post();
+    });
+    function validate(){
+        if ( $('#cm-cname').val() == '' )
+            /*TODO append validation error message*/
+            return false;
+        return true;
+    }
+    function handle_response(response){
+        // is json?
+        if ( typeof response.success != 'undefined'){
+            clean_all();
+            if ( response.success ) { /* Success!! */
+                $('#cm-ctheme').append(response.data.stylesheet);
+                $('#cm-preview').attr('href',
+                    $('#cm-preview').attr('href')+response.data.stylesheet
+                );
+            }else{
+                prepare_display_error(response.data.message);
+            }
+            $('#cm-success').css('display', 'block');
+        }else{
+            if ( response.indexOf("<form") > -1 ){ /*ftp*/
+                detach_ftp_form();
+                $('#cm-form-container').append('<div id="ftp-form">'+response+'</div>');
+                scroll_to('#ftp-form');
+                $('#childify-container input#upgrade').click(function(evt){
+                    evt.preventDefault();
+                    $(this).attr('disabled', 'disabled');
+                    post();        
+                });
+            }else{/* general error */
+                // handle 0 and -1 replies? don't think so
+                clean_all();
+                prepare_display_error(response);
+                $('#cm-success').css('display', 'block');
+            }
+        }
+    }
+    function prepare_display_error($message){
+        $('#cm-success > p').text( $message ? $message : "Error");
+        $('#cm-success').removeClass('updated');
+        $('#cm-success').addClass('error');
+        $('#cm-success #cm-preview').detach();
+    }
+    function post(){
+        data += "&"+$('#childify-container form').serialize();
+        $.post(ajaxurl, data, function(response){ 
+            handle_response(response); 
+        });
+    }
+    function clean_all(){
+       $('#cm-form-container').detach();
+       $('#cm-add-new').detach();
+       $('#cm-info').detach();
+    }
+    function detach_ftp_form(){
+        if ( $('#ftp-form').length > 0 ){
+            $('#ftp-form').detach();
+        }
+    }
+    function scroll_to($anchor){
+        var $offset = parseInt( $($anchor).offset().top ) - 
+            parseInt( $('.wp-full-overlay-sidebar-content').offset().top ) +
+            parseInt( $('.wp-full-overlay-sidebar-content').scrollTop() );
+        $('.wp-full-overlay-sidebar-content').animate({
+            scrollTop: $offset
+        }, 700);
+    }
+    $(document).ready(function($){
+        $('#childify-container').css('display', 'block');
+    });
+});
---
+++ childify-me.php
@@ -0,0 +1,281 @@
+<?php
+/**
+ * Plugin Name: Childify Me
+ * Plugin URI: https://github.com/eri-trabiccolo/childify-me
+ * Description: Create a child theme
+ * Version: 0.9.0
+ * Author: Rocco Aliberti
+ * Author URI: https://github.com/eri-trabiccolo
+ * License: GPL2+
+ */
+
+/**
+* Fires the plugin
+* @author Rocco Aliberti
+* @since 1.0
+*/
+/*
+ * TODO: 
+ * - handle errors on wp_filesystem->put_contents() ?
+ */
+if ( ! class_exists( 'Childify_Me' ) ) :
+class Childify_Me {
+    //Access any method or var of the class with classname::$instance -> var or method():
+    static $instance;
+    public $version;
+    public $plug_name;
+    public $plug_version;
+    public $plug_lang;
+
+    function __construct () {
+        self::$instance =& $this;
+        $this -> plug_name     = 'Childify Me';
+        $this -> plug_version  = '0.9.0';
+        $this -> plug_lang     = 'childify-me';
+
+        //USEFUL CONSTANTS
+        if ( ! defined( 'CM_DIR_NAME' ) ) { 
+            define( 'CM_DIR_NAME' , basename( dirname( __FILE__ ) ) ); 
+        }
+        if ( ! defined( 'CM_BASE_URL' ) ) {
+            define( 'CM_BASE_URL' , plugins_url( CM_DIR_NAME ) ); 
+        }
+        if ( ! defined( 'CM_CACTION' ) ) {
+            define( 'CM_CACTION' , 'cm_create' ); 
+        }
+
+        //adds plugin text domain
+        add_action( 'plugins_loaded', array( $this , 'cm_plugin_lang' ) );
+        //setup hooks
+        add_action( 'plugins_loaded', array( $this , 'cm_plugin_setup_hooks') );
+    }//end of construct
+
+    function cm_plugin_setup_hooks() {
+                
+        add_action ( 'customize_controls_enqueue_scripts', 
+            array( $this , 'cm_customize_js_css' ), 
+            100
+        );
+       
+        add_action ( 'customize_controls_print_footer_scripts', 
+            array( $this, 'print_template' ) 
+        ); 
+
+        add_action ('wp_ajax_'.CM_CACTION, 
+            array( $this, 'cm_create_child_theme')
+        );
+    }
+
+    // ajax callback
+    function cm_create_child_theme(){
+        if ( ! is_user_logged_in() )
+            wp_die( 0 );
+
+        check_ajax_referer( 'cm-nonce', 'CMnonce' );
+
+        if ( ! current_user_can( 'edit_theme_options' ) )
+            wp_die( -1 );
+        if ( isset($_POST['parent']) ){
+            if ( wp_get_theme($_POST['parent']) -> parent() )
+                wp_die( 0 );
+        }else
+            wp_die( 0 );
+
+        $this -> create_child_theme(
+            $_POST['parent'],
+            $_POST['cm-cname']
+        );
+        wp_die();
+    }
+
+    function create_child_theme($parent_stylesheet, $childname){
+        global $wp_filesystem;
+        if ( ( $creds = request_filesystem_credentials( '', '', false, 
+            get_theme_root(), null ) ) === false ) {
+                
+            return;
+		}
+		if ( ! WP_Filesystem( $creds, get_theme_root() ) ) {
+			request_filesystem_credentials( '', '', true, get_theme_root(), null );
+			return;
+		}
+
+        if ( ! ( $wp_filesystem instanceof WP_Filesystem_Base ) ) {
+			if ( ! WP_Filesystem() ) {
+                wp_send_json_error( array(
+                    'message' =>__( 'Error while trying to access to the filesystem!', 
+                    $this -> plug_lang )
+                ));
+                return;
+			}
+        }
+
+        $current_theme          = wp_get_theme( $parent_stylesheet );
+        $parent_stylesheet      = $current_theme -> get_stylesheet();
+        $parent_name            = $current_theme -> Name;
+        $i                      = 2;
+        $child                  = $childname;
+        $child_theme_directory  = trailingslashit( get_theme_root() ) . 
+            sanitize_file_name( strtolower( $child ) ) ;
+        $parent_theme_directory = trailingslashit( get_theme_root() ) . 
+            sanitize_file_name( $current_theme -> Template ) ;
+
+        /* incremental dirname */
+        $suffix                 = '';
+        while ( $wp_filesystem -> is_dir( $child_theme_directory . $suffix ) )
+            $suffix = '_' . $i++;
+          
+        $child                 .= $suffix;
+        $child_theme_directory .= $suffix;
+
+        $current_user           = wp_get_current_user();
+        $author                 = strlen($current_user -> user_firstname . 
+                                    $current_user -> user_lastname) > 0 ? 
+                $current_user -> user_firstname . " " . $current_user -> user_lastname : 
+                $current_user -> user_login;
+
+        $load_parent_stylesheet = ! in_array( $parent_stylesheet, 
+            array(
+                'customizr',
+                'customizr-pro') ) ? '*/
+@import url("../'.$parent_stylesheet.'/style.css");' : '*/';
+        
+        $wp_filesystem->mkdir( $child_theme_directory );
+        $child_stylesheet       = trailingslashit( $child_theme_directory ) . 'style.css';
+        $child_stylesheet_content = <<<EOF
+/*
+Theme Name: $child
+Version: 1.0
+Description: A child theme of $parent_name
+Template: $parent_stylesheet
+Author: $author
+$load_parent_stylesheet
+/* Your awesome customization starts here */
+EOF;
+        $wp_filesystem->put_contents( $child_stylesheet, $child_stylesheet_content );
+        $child_functionsphp_content = <<<EOF
+<?php
+/* Write your awesome functions below */
+EOF;
+
+        $child_functionsphp = trailingslashit( $child_theme_directory ) . 'functions.php';
+        $wp_filesystem->put_contents( $child_functionsphp, $child_functionsphp_content );
+        if ( file_exists( $parent_theme_directory . "/screenshot.png" ) ) {
+            $screenshot = $this->childify_screenshot(
+                $parent_theme_directory.'/screenshot.png');
+            $wp_filesystem->put_contents($child_theme_directory.'/screenshot.png', 
+                $screenshot);
+        }
+        wp_send_json_success( array(
+            'stylesheet' => sanitize_file_name( strtolower( $child ) )
+        ));
+        return;
+    }
+    
+    // create screenshot image(string): overlay of parent screenshot + childify-me badge
+    function childify_screenshot($screenshot){
+        $dest = imagecreatefrompng($screenshot);
+        $src = imagecreatefrompng( plugin_dir_path(__FILE__) . '/back/assets/img/child.png');
+        imagecopy($dest, $src, 20, 20, 0, 0, 130, 130);
+        ob_start();
+        header('Content-Type: image/png');
+        imagepng($dest);
+        $image = ob_get_contents();
+        ob_end_clean();
+        imagedestroy($src);
+        imagedestroy($dest);
+        return $image;
+    }
+   
+
+    // declares the plugin translation domain
+    function cm_plugin_lang() {
+        load_plugin_textdomain( $this -> plug_lang , false, CM_DIR_NAME . '/lang' );
+    }
+
+    function cm_customize_js_css() {
+        global $wp_customize;
+        $current_stylesheet = $wp_customize->theme()->stylesheet ;
+        if ( wp_get_theme($current_stylesheet) -> parent() )
+            return;
+
+        wp_enqueue_style(
+            'cm-customizer-style',
+            sprintf('%1$s/back/assets/css/cm-customizer%2$s.css' , 
+                CM_BASE_URL,
+                ( defined('WP_DEBUG') && true == WP_DEBUG ) ? '' : '.min'
+            ),
+	        array( 'customize-controls' ),
+            $this -> plug_version,
+            $media = 'all'
+        );
+        wp_enqueue_script(
+            'cm-customizer' ,
+            sprintf('%1$s/back/assets/js/cm-customizer%2$s.js' , 
+                CM_BASE_URL,
+                ( defined('WP_DEBUG') && true == WP_DEBUG ) ? '' : '.min'
+            ),
+            array( 'customize-controls', 'underscore' ),
+            $this -> plug_version,
+            true
+        );
+        wp_localize_script(
+           'cm-customizer',
+           'CMAdmin',
+            array(
+                'AjaxUrl'    => is_multisite() ? network_admin_url( 'admin-ajax.php' ):
+                                                 admin_url( 'admin-ajax.php' ),
+                'CMnonce' 	 => wp_create_nonce( 'cm-nonce' ),
+                'Action'     => CM_CACTION,
+                'Parent'     => $current_stylesheet
+            )
+        ); 
+    }
+
+    function print_template(){
+    ?>
+        <script type="text/template" id="childify-tpl"> 
+            <div id="childify-container">
+              <?php
+                printf('<span id="cm-info" class="cm-notice">%1$s</span>',
+                    __('Click on the button below to create a child theme', $this::$instance->plug_lang )
+                );
+                printf('<span id="cm-add-new" class="cm-add-new button button-primary">%1$s
+                    </span>',
+                    "Childify Me"
+                );
+                printf('
+                    <div id="cm-form-container" style="display:none">
+                        <form id="cm-form">
+
+                            <input placeholder="%1$s" type="text" id="cm-cname" name="cm-cname" value="">
+                        </form>
+                        <div id="cm-actions">
+                            <span id="cm-create" class="button button-secondary">%2$s</span><span class="button button-secondary" id="cm-cancel">%3$s</span>
+                        </div>
+                    </div>',
+                    __( "Child theme name here" , $this::$instance -> plug_lang ),
+                    __( "Create" , $this::$instance -> plug_lang ),
+                    __( "Cancel" , $this::$instance -> plug_lang )
+                );
+                printf('<div id="cm-success" class="updated"><p>%1$s <span id="cm-ctheme"></span> %2$s</p><a id="cm-preview" class="button button-primary" href="%3$s" title="%4$s">%4$s</a></div>',
+                    __("Child theme", $this::$instance -> plug_lang ),
+                    __("successfully created!", $this::$instance -> plug_lang ),
+                    sprintf('%1$s?theme=',
+                        is_multisite() ? network_admin_url( 'customize.php' ) :
+                        admin_url( 'customize.php' )
+                    ),
+                    __("Preview and Activate", $this::$instance -> plug_lang )
+                );
+              ?>
+          </div>
+       </script>
+    <?php
+    }
+
+}//end of class
+
+//Creates a new instance of front and admin
+new Childify_Me;
+
+endif;
