df6bb80f7d1a95d4615a55545d51c459398d4bf2
Rocco Aliberti <raliberti84@gmail.com>
2015-03-26 17:19:40 +0100

    Fix: version bump.

--- childify-me.php
+++ childify-me.php
@@ -3,7 +3,7 @@
  * Plugin Name: Childify Me
  * Plugin URI: https://github.com/eri-trabiccolo/childify-me
  * Description: Create a child theme from the Theme Customizer panel
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: Rocco Aliberti
  * Author URI: https://github.com/eri-trabiccolo
  * License: GPL2+
