95acce948c6c19dab503d8e8424ddae4f642eba3
Rocco Aliberti <raliberti84@gmail.com>
2016-05-03 17:03:09 +0200

    Imp: add Hueman special treatment (do not import its style.css)

--- childify-me.php
+++ childify-me.php
@@ -15,7 +15,7 @@
 * @since 1.0.0
 */
 /*
- * TODO: 
+ * TODO:
  * - handle errors on wp_filesystem->put_contents() ?
  */
 if ( ! class_exists( 'Childify_Me' ) ) :
@@ -26,6 +26,13 @@ class Childify_Me {
     public $plug_version;
     public $plug_lang;
 
+    //themes which do not need the style.css importing
+    public $special_themes = array(
+      'customizr' => array(),
+      'customizr-pro' => array(),
+      'hueman' => array( 'version' => '3.0' )
+    );
+
     function __construct () {
         self::$instance =& $this;
         $this -> plug_name     = 'Childify Me';
@@ -33,14 +40,14 @@ class Childify_Me {
         $this -> plug_lang     = 'childify-me';
 
         //USEFUL CONSTANTS
-        if ( ! defined( 'CM_DIR_NAME' ) ) { 
-            define( 'CM_DIR_NAME' , basename( dirname( __FILE__ ) ) ); 
+        if ( ! defined( 'CM_DIR_NAME' ) ) {
+            define( 'CM_DIR_NAME' , basename( dirname( __FILE__ ) ) );
         }
         if ( ! defined( 'CM_BASE_URL' ) ) {
-            define( 'CM_BASE_URL' , plugins_url( CM_DIR_NAME ) ); 
+            define( 'CM_BASE_URL' , plugins_url( CM_DIR_NAME ) );
         }
         if ( ! defined( 'CM_CACTION' ) ) {
-            define( 'CM_CACTION' , 'cm_create' ); 
+            define( 'CM_CACTION' , 'cm_create' );
         }
 
         //adds plugin text domain
@@ -50,17 +57,17 @@ class Childify_Me {
     }//end of construct
 
     function cm_plugin_setup_hooks() {
-                
-        add_action ( 'customize_controls_enqueue_scripts', 
-            array( $this , 'cm_customize_js_css' ), 
+
+        add_action ( 'customize_controls_enqueue_scripts',
+            array( $this , 'cm_customize_js_css' ),
             100
         );
-       
-        add_action ( 'customize_controls_print_footer_scripts', 
-            array( $this, 'print_template' ) 
-        ); 
 
-        add_action ('wp_ajax_'.CM_CACTION, 
+        add_action ( 'customize_controls_print_footer_scripts',
+            array( $this, 'print_template' )
+        );
+
+        add_action ('wp_ajax_'.CM_CACTION,
             array( $this, 'cm_create_child_theme')
         );
     }
@@ -89,9 +96,9 @@ class Childify_Me {
 
     function create_child_theme($parent_stylesheet, $childname){
         global $wp_filesystem;
-        if ( ( $creds = request_filesystem_credentials( '', '', false, 
+        if ( ( $creds = request_filesystem_credentials( '', '', false,
             get_theme_root(), null ) ) === false ) {
-                
+
             return;
 		}
 		if ( ! WP_Filesystem( $creds, get_theme_root() ) ) {
@@ -102,7 +109,7 @@ class Childify_Me {
         if ( ! ( $wp_filesystem instanceof WP_Filesystem_Base ) ) {
 			if ( ! WP_Filesystem() ) {
                 wp_send_json_error( array(
-                    'message' =>__( 'Error while trying to access the filesystem!', 
+                    'message' =>__( 'Error while trying to access the filesystem!',
                     $this -> plug_lang )
                 ));
                 return;
@@ -110,11 +117,12 @@ class Childify_Me {
         }
 
         $current_theme          = wp_get_theme( $parent_stylesheet );
+
         $parent_stylesheet      = $current_theme -> get_stylesheet();
         $parent_name            = $current_theme -> Name ? $current_theme -> Name :
                                                            $parent_stylesheet;
         $child                  = $childname;
-        $child_theme_directory  = trailingslashit( get_theme_root() ) . 
+        $child_theme_directory  = trailingslashit( get_theme_root() ) .
             sanitize_file_name( strtolower( $child ) ) ;
         $parent_theme_directory = $current_theme -> get_stylesheet_directory();
 
@@ -123,22 +131,31 @@ class Childify_Me {
         $suffix                 = '';
         while ( $wp_filesystem -> is_dir( $child_theme_directory . $suffix ) )
             $suffix = '_' . $i++;
-          
+
         $child                 .= $suffix;
         $child_theme_directory .= $suffix;
 
         $current_user           = wp_get_current_user();
-        $author                 = strlen($current_user -> user_firstname . 
-                                    $current_user -> user_lastname) > 0 ? 
-                trim( $current_user -> user_firstname . " " . $current_user -> user_lastname ) : 
+        $author                 = strlen($current_user -> user_firstname .
+                                    $current_user -> user_lastname) > 0 ?
+                trim( $current_user -> user_firstname . " " . $current_user -> user_lastname ) :
                 __( 'Administrator', $this -> plug_lang );
 
-        $load_parent_stylesheet = ! in_array( $parent_stylesheet, 
-            array(
-                'customizr',
-                'customizr-pro') ) ? '*/
+        /* Do we need to import the parent style.css ? */
+        $_has_parent_stylesheet_to_load = true;
+        if ( array_key_exists( $parent_stylesheet, $this -> special_themes ) ) {
+          $special_themes = $this -> special_themes;
+          if ( ! isset( $special_themes[ $parent_stylesheet ][ 'version' ] ) ||
+              ( isset( $special_themes[ $parent_stylesheet ][ 'version' ] ) &&
+                   version_compare( $special_themes[ $parent_stylesheet ][ 'version' ], $current_theme -> Version, '<=' ) )
+          )
+            $_has_parent_stylesheet_to_load = false;
+        }
+
+        $load_parent_stylesheet = $_has_parent_stylesheet_to_load ?
+            '*/
 @import url("../'.$parent_stylesheet.'/style.css");' : '*/';
-        
+
         $wp_filesystem -> mkdir( $child_theme_directory );
         $child_stylesheet       = trailingslashit( $child_theme_directory ) . 'style.css';
         $child_stylesheet_content = <<<EOF
@@ -160,12 +177,12 @@ EOF;
         $child_functionsphp = trailingslashit( $child_theme_directory ) . 'functions.php';
         $wp_filesystem -> put_contents( $child_functionsphp, $child_functionsphp_content );
 
-        /* create the child-theme screenshot.png*/    
+        /* create the child-theme screenshot.png*/
         if ( file_exists( $parent_theme_directory . '/screenshot.png' ) ) {
             if ( extension_loaded( 'gd' ) && function_exists( 'gd_info' ) ){
                 $screenshot = $this -> childify_screenshot(
                     $parent_theme_directory . '/screenshot.png');
-                $wp_filesystem -> put_contents( $child_theme_directory . '/screenshot.png', 
+                $wp_filesystem -> put_contents( $child_theme_directory . '/screenshot.png',
                     $screenshot );
             }else
                 $wp_filesystem -> copy( $parent_theme_directory . "/screenshot.png",
@@ -177,13 +194,13 @@ EOF;
         ));
         return;
     }//end of create_child_theme()
-    
+
     //create screenshot image(string): overlay of parent screenshot + childify-me badge
     function childify_screenshot( $screenshot ){
         $parent_src = imagecreatefrompng($screenshot);
         $cm_src = imagecreatefrompng( plugin_dir_path(__FILE__) .
                                         '/back/assets/img/child.png' );
-        
+
         if ( function_exists('imagecreatetruecolor') ){
             $dest = imagecreatetruecolor(880, 660);
             imagecopy( $dest, $parent_src, 0, 0, 0, 0, 880, 660 );
@@ -191,23 +208,23 @@ EOF;
             $dest = $parent_src;
 
         imagecopy( $dest, $cm_src, 0, 0, 0, 0, 350, 350 );
-        
+
         ob_start();
             header( 'Content-Type: image/png' );
             imagepng( $dest );
         $image = ob_get_contents();
         ob_end_clean();
-        
+
         imagedestroy( $cm_src );
         imagedestroy( $dest );
-        
+
         if ( $dest !== $parent_src ){
             imagedestroy( $parent_src );
         }
 
         return $image;
     }
-   
+
     //declares the plugin translation domain
     function cm_plugin_lang() {
         load_plugin_textdomain( $this -> plug_lang , false, CM_DIR_NAME . '/lang' );
@@ -221,7 +238,7 @@ EOF;
 
         wp_enqueue_style(
             'cm-customizer-style',
-            sprintf('%1$s/back/assets/css/cm-customizer%2$s.css' , 
+            sprintf('%1$s/back/assets/css/cm-customizer%2$s.css' ,
                 CM_BASE_URL,
                 ( defined('WP_DEBUG') && true == WP_DEBUG ) ? '' : '.min'
             ),
@@ -231,7 +248,7 @@ EOF;
         );
         wp_enqueue_script(
             'cm-customizer' ,
-            sprintf('%1$s/back/assets/js/cm-customizer%2$s.js' , 
+            sprintf('%1$s/back/assets/js/cm-customizer%2$s.js' ,
                 CM_BASE_URL,
                 ( defined('WP_DEBUG') && true == WP_DEBUG ) ? '' : '.min'
             ),
@@ -248,13 +265,13 @@ EOF;
                 'Action'     => CM_CACTION,
                 'Parent'     => $current_stylesheet
             )
-        ); 
+        );
     }
-    
+
     //this template will be loaded with underscore in cm-customizr(.min).js
     function print_template(){
     ?>
-        <script type="text/template" id="childify-tpl"> 
+        <script type="text/template" id="childify-tpl">
             <div id="childify-container">
               <?php
                 printf('<span id="cm-info" class="cm-notice">%1$s</span>',
@@ -281,7 +298,7 @@ EOF;
                 printf('<div id="cm-success" class="updated"><p>%1$s <span id="cm-ctheme"></span> %2$s</p>%3$s</div>',
                     __("Child theme", $this -> plug_lang ),
                     __("successfully created!", $this -> plug_lang ),
-                    ( ! is_multisite() ) ? 
+                    ( ! is_multisite() ) ?
                         sprintf('<a id="%3$s" class="button button-primary" href="%1$s" title="%2$s" tabindex="0">%2$s</a>',
                             sprintf('%1$s?theme=', admin_url( 'customize.php' ) ),
                             __("Preview and Activate", $this -> plug_lang ),
